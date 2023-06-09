6af67e42153e2df8e8bc8de1ad0022bd76efe092
Thai Phan <tvphan360@gmail.com>
2023-06-09 15:57:17 -0400

    Replace bespoke axios calls with Redux data fetching calls

--- app/src/pages/BookingsList.tsx
+++ app/src/pages/BookingsList.tsx
@@ -1,53 +1,45 @@
-import { axios } from '../common'
-import { useEffect, useState } from 'react'
-import { Booking } from '../common'
-import moment from 'moment'
-
+import moment from "moment";
+import {
+  useDeleteBookingMutation,
+  useGetBookingsQuery,
+} from "../store/apis/fixerupper";
 
 export default function BookingsList() {
-  const [bookings, setBookings] = useState<Booking[]>([])
-  
-  useEffect(() => {
-    axios
-    .get('http://localhost:4000/api/getBookings')
-    .then(({ data }) => {
-      setBookings(data)
-    })
-  }, [])
+  const { data: bookings } = useGetBookingsQuery();
 
-  return <>
-    <div className="row">
-      <div className="col h4">
-        Booking Date
+  const [deleteBooking] = useDeleteBookingMutation();
+
+  return (
+    <>
+      <div className="row">
+        <div className="col h4">Booking Date</div>
+        <div className="col h4">Booking Location</div>
+        <div className="col h4">Booked By</div>
+        <div className="col"></div>
       </div>
-      <div className="col h4">
-        Booking Location
-      </div>
-      <div className="col h4">
-        Booked By
-      </div>
-      <div className="col"></div>
-    </div>
-    {bookings.map((booking) => {
-      return (<div key={booking.id} className="row" style={{border: "1px solid black"}}>
-        <div className="col">
-          {moment(booking.bookingDate).toDate().toDateString()}
-        </div>
-        <div className="col">
-          {booking.location}
-        </div>
-        <div className="col">
-          {booking.username}
-        </div>
-        <div 
-          className="col btn btn-success" 
-          onClick={() => {
-            axios
-              .get(`/deleteBooking?id=${booking.id}`)
-          }}> 
-          Delete
-        </div>
-      </div>)
-    })}
-  </>
+      {bookings?.map((booking) => {
+        return (
+          <div
+            key={booking.id}
+            className="row"
+            style={{ border: "1px solid black" }}
+          >
+            <div className="col">
+              {moment(booking.bookingDate).toDate().toDateString()}
+            </div>
+            <div className="col">{booking.location}</div>
+            <div className="col">{booking.username}</div>
+            <div
+              className="col btn btn-success"
+              onClick={() => {
+                deleteBooking(booking.id);
+              }}
+            >
+              Delete
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
--- app/src/store/apis/fixerupper.ts
+++ app/src/store/apis/fixerupper.ts
@@ -4,11 +4,23 @@ import { Booking } from '../../common';
 export const fixerupperApi = createApi({
   reducerPath: 'fixerupperApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/api/' }),
+  tagTypes: ['Booking'],
   endpoints: builder => ({
     getBookings: builder.query<Booking[], void>({
-      query: () => 'getBookings'
+      query: () => 'getBookings',
+      providesTags: ['Booking']
+    }),
+    deleteBooking: builder.mutation<{ message: string }, string>({
+      query: (id) => ({
+        method: 'GET',
+        url: 'deleteBooking',
+        params: {
+          id
+        }
+      }),
+      invalidatesTags: ['Booking']
     })
   })
 })
 
-export const { useGetBookingsQuery } = fixerupperApi
+export const { useGetBookingsQuery, useDeleteBookingMutation } = fixerupperApi
